ebd54108e351ecb47b8d00232209941fa4d0c003
Sarthak Khillon <sarthakkhillon24@gmail.com>
2019-01-30 20:25:14 -0800

    Initial Commit

---
+++ NotHotdogSolution/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NotHotdogSolution
+//
+//  Created by Sarthak Khillon on 1/30/19.
+//  Copyright © 2019 Sarthak Khillon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
